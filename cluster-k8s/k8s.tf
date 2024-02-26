77977221b89b8cb91261b167563720fe9651c4fc
localadm <tulamelkii@yandex.ru>
2024-02-27 02:40:50 +0300

    add string

--- cluster-k8s/k8s.tf
+++ cluster-k8s/k8s.tf
@@ -14,6 +14,7 @@ provider "yandex" {
 resource "yandex_vpc_network" "default" {
         name   = "vpc.k8s.network"
    description = "virtual network cluster k8s for ru-central1-a"
+
        }
 
 resource "yandex_vpc_subnet" "cluster-subnet-a" {
@@ -21,6 +22,6 @@ resource "yandex_vpc_subnet" "cluster-subnet-a" {
    description = "subnet for cluster k8s"
 v4_cidr_blocks = ["192.168.49.0/28"]
           zone = "ru-central1-a"
-    network_id = "enpsm794i9o9rhvshgvi"
+    network_id = yandex_vpc_network.default.id
 }
 
