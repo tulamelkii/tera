4bce5af3e8c0963569c85a5e322d8dd80d62f2f3
localadm <tulamelkii@yandex.ru>
2024-02-27 23:28:40 +0300

    group instance

--- cluster-k8s/k8s.tf
+++ cluster-k8s/k8s.tf
@@ -15,9 +15,9 @@ resource "yandex_vpc_network" "default" {
         name   = "vpc.k8s.network"
    description = "virtual network cluster k8s for ru-central1-a"
 
-       }
+      }
 
-resource "yandex_vpc_subnet" "cluster-subnet-a" {
+resource "yandex_vpc_subnet" "vpc.k8s.subnet" {
          name  = "vpc.k8s.subnet"
    description = "subnet for cluster k8s"
 v4_cidr_blocks = ["192.168.49.0/28"]
@@ -25,3 +25,18 @@ v4_cidr_blocks = ["192.168.49.0/28"]
     network_id = yandex_vpc_network.default.id
 }
 
+resource "yandex_iam_service_account" "localadm" {
+          name = "localadm"
+  descriptions = "service account ks8"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "admin" {
+     folder_id = "b1gahuuq85502ap2q4im" 
+          role = "admin"
+        member = 
+
+resource "yandex_compute_instance_group" "control-group" {
+          name = "control-group"
+     folder_id = "b1gahuuq85502ap2q4im"
+     
+     
